8000ada01ba4c31548287a0d57a46cc5adaf0152
Matheus Alves <matheuseejam@gmail.com>
2023-02-26 17:32:36 -0300

    Remove pastas

--- components/cleytin_engine/ce_canvas.cpp
+++
@@ -1,9 +0,0 @@
-#include "ce_canvas.h"
-
-void CECanvas::setBackgroundColor(const CEColor color) {
-    this->backgroundColor = color;
-}
-
-CEColor CECanvas::getBackgroundColor() {
-    return this->backgroundColor;
-}
--- components/cleytin_engine/ce_font.cpp
+++
@@ -1,7 +0,0 @@
-#include "ce_font.h"
-
-CEFont::~CEFont() {/* EMPTY */}
-
-char *CEFont::parseString(const char *str) {
-    return utf8ToLatin1(str);
-}
